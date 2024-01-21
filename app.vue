2dd0d9f61dc54e0b260836d70d9f917fcbd7d894
MikeSymoDev <mike04_norcal@yahoo.de>
2024-01-21 16:04:50 +0100

    nuxt and storyblok setup

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>this is a test</h1>
+    <pre>{{ story }}</pre>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="js">
+const story = await useStoryblok('home', { version: 'draft'})
+</script>
